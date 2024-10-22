794ea12798a9b30f384dd8c4589d51eb3146a5a3
vloiseau13 <valentin.loiseau13@gmail.com>
2024-10-22 11:17:03 +0200

    PokemonCard

---
+++ my-react-app/src/componants/PokemonCard.tsx
@@ -0,0 +1,11 @@
+function PokemonCard() {
+  return (
+    <div>
+      <figure>
+        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"></img>
+        <figcaption>Bulbizarre</figcaption>
+      </figure>
+    </div>
+  );
+}
+export default PokemonCard;
