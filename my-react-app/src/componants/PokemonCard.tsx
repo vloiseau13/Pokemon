1e7bc19f1a1775809ffc1d58b58e376be7fc4923
vloiseau13 <valentin.loiseau13@gmail.com>
2024-10-22 11:26:06 +0200

    componants 2

--- my-react-app/src/componants/PokemonCard.tsx
+++ my-react-app/src/componants/PokemonCard.tsx
@@ -1,3 +1,14 @@
+const pokemonList = [
+  {
+    name: "bulbasaur",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "mew",
+  },
+];
+
 function PokemonCard() {
   return (
     <div>
