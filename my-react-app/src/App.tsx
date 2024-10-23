1b05b53c5d257733513f347d4ad4aab1a33629da
vloiseau13 <valentin.loiseau13@gmail.com>
2024-10-23 09:45:32 +0200

    Minor change

--- my-react-app/src/App.tsx
+++ my-react-app/src/App.tsx
@@ -1,22 +1,11 @@
 import "./App.css";
 import PokemonCard from "./componants/PokemonCard";
 
-const pokemonList = [
-  {
-    name: "Bulbizarre",
-    imgSrc:
-      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-  },
-  {
-    name: "Mew",
-  },
-];
-
 function App() {
   return (
-    <div>
-      <PokemonCard pokemon={pokemonList[0]} />
-    </div>
+    <>
+      <PokemonCard />
+    </>
   );
 }
 
