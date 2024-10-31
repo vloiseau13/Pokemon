8317a33aa39d17c26134a1a71e538f2f93e5cb47
vloiseau13 <valentin.loiseau13@gmail.com>
2024-10-31 15:19:25 +0100

    Nouvel essai

--- my-react-app/src/App.tsx
+++ my-react-app/src/App.tsx
@@ -1,10 +1,22 @@
 import "./App.css";
 import PokemonCard from "./componants/PokemonCard";
 
+const pokemonList = [
+  {
+    name: "Bulbizarre",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "Mew",
+  },
+];
+
 function App() {
+  const pokemon = { name: "mew", imgSrc: null };
   return (
     <>
-      <PokemonCard />
+      <PokemonCard pokemon={pokemon} />
     </>
   );
 }
--- my-react-app/src/componants/PokemonCard.tsx
+++ my-react-app/src/componants/PokemonCard.tsx
@@ -1,14 +1,3 @@
-const pokemonList = [
-  {
-    name: "Bulbizarre",
-    imgSrc:
-      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-  },
-  {
-    name: "Mew",
-  },
-];
-
 function PokemonCard({ pokemon }) {
   return (
     <div>
